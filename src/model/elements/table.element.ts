1c4ccd59c7e67f562cef1af5d578eb9fe864bf86
xn0-MM <chanomm75@gmail.com>
2025-11-26 13:41:14 +0100

    refactor(feature/new-features): added last changes

--- src/model/elements/table.element.ts
+++ src/model/elements/table.element.ts
@@ -1,5 +1,7 @@
 import { Locator, Page } from '@playwright/test';
 import { BaseElement } from './base.element';
+import { ICustomerData } from '../agents/customer.agent';
+import { CustomerDataMapper } from '@/utils/data.mapper';
 
 export class Table extends BaseElement {
 
@@ -24,25 +26,32 @@ export class Table extends BaseElement {
         return this.rows.nth(rowIndex);
     }
 
+    async getRows(): Promise<Locator[]> {
+        return await this.rows.all();
+    }
+
     async getHeaderMessage(): Promise<string> {
         const headerCell = this.thead
         return (await headerCell.innerText()).trim();
     }
 
-    async getTableData() {
+    private async getRawTableData(): Promise<string[][]> {
         await this.waitForVisible();
-        const rows = await this.rows.all();
-        const entries: Record<string, string> = {};
+
+        const rows = await this.getRows()
+        const rawData: string[][] = [];
 
         for (const row of rows) {
             const cells = await row.locator('td').all();
-            if (cells.length === 2) {
-                const key = (await cells[0].innerText()).trim();
-                const value = (await cells[1].innerText()).trim();
-                entries[key] = value;
-            }
+            const values = await Promise.all(cells.map(cell => cell.innerText()));
+            rawData.push(values.map(v => v.trim()));
         }
 
-        return entries;
+        return rawData;
+    }
+
+    async getTableData(): Promise<ICustomerData> {
+        const data = await this.getRawTableData();
+        return CustomerDataMapper.getMappedData(data)
     }
 }
